9654ce628eeef34735081046cd0fb49107845c4e
prafpawar11 <74486812+prafpawar11@users.noreply.github.com>
2022-02-15 20:28:16 +0530

    Update AdminPage.java
    Add Code by Akshay

--- src/Demo1/AdminPage.java
+++ src/Demo1/AdminPage.java
@@ -4,6 +4,7 @@ public class AdminPage {
 	
 	public static void main(String[] args) {
 		System.out.println("Admin Page Functionality");
+		System.out.println("Admin Page Add Employee Functionality");
 	}
 
 }
